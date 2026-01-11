c77afa35e069e9e2dfba86e4709eb746fc7076b0
kaxadeveloper <154353756+kaxadeveloper@users.noreply.github.com>
2026-01-11 13:30:42 +0400

    jquery

---
+++ app.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+   $('i').click(function(){
+      $('ul').toggleClass('ul_show');
+      $('section').toggleClass('slide_image');
+   });
+   $('li').click(function(){
+      $(this).addClass('active').siblings().removeClass('active');
+   });
+});
